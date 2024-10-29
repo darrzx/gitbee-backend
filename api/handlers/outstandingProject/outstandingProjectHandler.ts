cc924c23062ccf438e100b020cbe7105e1744271
darrzx <darrenarianto0406@gmail.com>
2024-10-29 21:22:50 +0700

    add get all outstanding project endpoint

--- api/handlers/outstandingProject/outstandingProjectHandler.ts
+++ api/handlers/outstandingProject/outstandingProjectHandler.ts
@@ -7,5 +7,64 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default class OutstandingProjectHandler { 
+    static async getAllOutstandingProject(req : Request, res : Response, next : NextFunction) {
+        try {
+            const outstandingProjects = await prisma.outstandingProject.findMany({
+                include: {
+                    project: {
+                        include: {
+                            projectDetail: true,
+                            projectGroups: true,
+                            galleries: true,
+                            projectTechnologies: true
+                        }
+                    }
+                }
+            });
 
+            const updatedOutstandingProjects = await Promise.all(outstandingProjects.map(async (outstandingProject) => {
+                const { project_id, ...updatedOutstandingProject } = outstandingProject;
+
+                const project = outstandingProject.project;
+                const { created_at, ...updatedProject } = project;
+    
+                const updatedProjectGroups = project.projectGroups.map(group => {
+                    const { id, project_id, ...otherAttributes } = group;
+                    return otherAttributes;
+                });
+    
+                const updatedGalleries = project.galleries.map(gallery => {
+                    const { id, project_id, ...otherAttributes } = gallery;
+                    return otherAttributes;
+                });
+    
+                const updatedProjectTechnologies = await Promise.all(
+                    project.projectTechnologies.map(async (technology) => {
+                        const technologyDetails = await prisma.technology.findUnique({
+                            where: { id: technology.technology_id }
+                        });
+                        const { id, project_id, ...otherAttributes } = technology;
+                        return {
+                            ...otherAttributes,
+                            technology_name: technologyDetails ? technologyDetails.name : null
+                        };
+                    })
+                );
+    
+                return {
+                    ...updatedOutstandingProject,
+                    project: {
+                        ...updatedProject,
+                        projectGroups: updatedProjectGroups,
+                        galleries: updatedGalleries,
+                        projectTechnologies: updatedProjectTechnologies
+                    }
+                };
+            }));
+    
+            sendSuccessResponse(res, updatedOutstandingProjects);
+        } catch (error) {
+            sendErrorResponse(res, error.message ? error.message : "Fetch Failed");
+        }
+    }
 }
--- api/routes/outstandingProject/outstandingProjectRoutes.ts
+++ api/routes/outstandingProject/outstandingProjectRoutes.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import OutstandingProjectHandler from "api/handlers/outstandingProject/outstandingProjectHandler";
 import hopOutstandingProjectRoutes from "./hop/hopOutstandingProjectRoutes";
 
 const outstandingProjectRoutes = express.Router();
+
+outstandingProjectRoutes.get("/all", OutstandingProjectHandler.getAllOutstandingProject);
 
 outstandingProjectRoutes.use("/hop", hopOutstandingProjectRoutes);
 

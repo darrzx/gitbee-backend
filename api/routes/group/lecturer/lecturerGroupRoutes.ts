a8669df1b8c5f6d1d978b3ba12c43a5dc16257ef
darrzx <darrenarianto0406@gmail.com>
2024-12-19 11:58:11 +0700

    fix bug

--- api/routes/group/lecturer/lecturerGroupRoutes.ts
+++ api/routes/group/lecturer/lecturerGroupRoutes.ts
@@ -4,7 +4,7 @@ import LecturerGroupHandler from "api/handlers/group/lecturer/lecturerGroupHandl
 const lecturerGroupRoutes = express.Router();
 
 lecturerGroupRoutes.get("/class-group", LecturerGroupHandler.getClassGroup);
-lecturerGroupRoutes.get("/class-list", LecturerGroupHandler.getClassGroup);
+lecturerGroupRoutes.get("/class-list", LecturerGroupHandler.getClassList);
 
 lecturerGroupRoutes.patch("/remove", LecturerGroupHandler.removeTemporaryGroup);
 
